cea5687192053c20af519ea96ffdda2fa7347575
Donald Gray <donald.gray@digirati.com>
2020-06-30 16:59:10 +0100

    Add health checks

--- src/Wellcome.Dds/Wellcome.Dds.Server/Startup.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Server/Startup.cs
@@ -1,15 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 using Wellcome.Dds.Server.Infrastructure;
 
 namespace Wellcome.Dds.Server
@@ -23,6 +15,8 @@ namespace Wellcome.Dds.Server
             services.AddControllers();
 
             services.AddSwagger();
+
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +32,7 @@ namespace Wellcome.Dds.Server
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapHealthChecks("/management/healthcheck");
             });
         }
     }
