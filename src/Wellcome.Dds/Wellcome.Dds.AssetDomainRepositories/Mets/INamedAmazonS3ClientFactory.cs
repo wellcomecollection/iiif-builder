0a54a706b8c7920cdd4a4a0c96e05f56f94cc3ab
Donald Gray <donald.gray@digirati.com>
2020-07-06 14:49:34 +0100

    Handle AWS interface implementations
    Add appsettings.staging.json

---
+++ src/Wellcome.Dds/Wellcome.Dds.AssetDomainRepositories/Mets/INamedAmazonS3ClientFactory.cs
@@ -0,0 +1,24 @@
+﻿using Amazon.S3;
+
+namespace Wellcome.Dds.AssetDomainRepositories.Mets
+{
+    /// <summary>
+    /// Interface to support managing named <see cref="IAmazonS3"/> clients.
+    /// </summary>
+    public interface INamedAmazonS3ClientFactory
+    {
+        /// <summary>
+        /// Get a previously registered named IAmazonS3 client.
+        /// </summary>
+        /// <param name="name">Name of client to fetch.</param>
+        /// <returns>IAmazonS3 client.</returns>
+        IAmazonS3 Get(string name);
+        
+        /// <summary>
+        /// Add a named IAmazonS3Client.
+        /// </summary>
+        /// <param name="name">Name of client.</param>
+        /// <param name="client">IAmazonS3Client implementation.</param>
+        void Add(string name, IAmazonS3 client);
+    }
+}
--- src/Wellcome.Dds/Wellcome.Dds.Dashboard/Program.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Dashboard/Program.cs
@@ -1,11 +1,7 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace Wellcome.Dds.Dashboard
 {
@@ -13,7 +9,22 @@ namespace Wellcome.Dds.Dashboard
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateLogger();
+            try
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Application start-up failed");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
