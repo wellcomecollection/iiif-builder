117b8bddad89436e1d6e0bcaea2f0d1288d3d124
Tom Crane <tom@nanoplanet.com>
2020-07-07 10:56:24 +0100

    reorganise IStorage implementations

--- src/Wellcome.Dds/Utils/Storage/FileSystem/FileSystemStorage.cs
+++ src/Wellcome.Dds/Utils/Storage/FileSystem/FileSystemStorage.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
-namespace Utils.Storage.StorageImpl
+namespace Utils.Storage.FileSystem
 {
     public class FileSystemStorage : IStorage
     {
