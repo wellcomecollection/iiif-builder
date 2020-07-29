6ecce2a2d5103172d89e022690fbac867b7c31af
Donald Gray <donald.gray@digirati.com>
2020-07-29 12:42:26 +0100

    Output PDF path with presentation

--- src/Wellcome.Dds/DlcsWebClient/Config/DlcsOptions.cs
+++ src/Wellcome.Dds/DlcsWebClient/Config/DlcsOptions.cs
@@ -13,6 +13,7 @@
         public string PortalPageTemplate { get; set; }
         public string PortalBatchTemplate { get; set; }
         public string SkeletonNamedQueryTemplate { get; set; }
+        public string SkeletonNamedPdfTemplate { get; set; }
         public bool PreventSynchronisation { get; set; } = false;
         public string PdfQueryName { get; set; } = "pdf";
 
