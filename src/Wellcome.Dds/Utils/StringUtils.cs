dacf3c116e7d66291781f0203afe0bd7f15ba12a
Donald Gray <donald.gray@digirati.com>
2020-08-06 10:55:58 +0100

    Handle links that haven't been migrated

--- src/Wellcome.Dds/Utils/StringUtils.cs
+++ src/Wellcome.Dds/Utils/StringUtils.cs
@@ -225,22 +225,22 @@ namespace Utils
             // from http://stackoverflow.com/a/36191436
             if (t.TotalSeconds <= 1)
             {
-                return string.Format(@"{0:s\.ff} seconds", t);
+                return $@"{t:s\.ff} seconds";
             }
             if (t.TotalMinutes <= 1)
             {
-                return string.Format(@"{0:%s} seconds", t);
+                return $@"{t:%s} seconds";
             }
             if (t.TotalHours <= 1)
             {
-                return string.Format(@"{0:%m} minutes", t);
+                return $@"{t:%m} minutes";
             }
             if (t.TotalDays <= 1)
             {
-                return string.Format(@"{0:%h} hours", t);
+                return $@"{t:%h} hours";
             }
 
-            return string.Format(@"{0:%d} days", t);
+            return $@"{t:%d} days";
         }
         
         /// <summary> 
