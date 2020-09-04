20b55ae19f5382d0377e80128ff5bb81ffe999b8
tomcrane <tom@nanoplanet.com>
2020-09-04 12:12:12 +0100

    Add remaining fields to Manifestation class

---
+++ src/Wellcome.Dds/Wellcome.Dds.Repositories/Migrations/20200904110725_AllFieldsInManifestations.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wellcome.Dds.Repositories.Migrations
+{
+    public partial class AllFieldsInManifestations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "asset_type",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "dip_status",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "dlcs_asset_type",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "error_message",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "file_count",
+                table: "manifestations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_file_extension",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_file_name",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "first_file_storage_identifier",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "index",
+                table: "manifestations",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "is_all_open",
+                table: "manifestations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "manifestation_file",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "manifestation_file_modified",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "package_file",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "package_file_modified",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "package_label",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "permitted_operations",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "poster_image",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "root_section_access_condition",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "root_section_type",
+                table: "manifestations",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "supports_search",
+                table: "manifestations",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "volume_identifier",
+                table: "manifestations",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "asset_type",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "dip_status",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "dlcs_asset_type",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "error_message",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "file_count",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "first_file_extension",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "first_file_name",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "first_file_storage_identifier",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "index",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "is_all_open",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "manifestation_file",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "manifestation_file_modified",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "package_file",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "package_file_modified",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "package_label",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "permitted_operations",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "poster_image",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "root_section_access_condition",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "root_section_type",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "supports_search",
+                table: "manifestations");
+
+            migrationBuilder.DropColumn(
+                name: "volume_identifier",
+                table: "manifestations");
+        }
+    }
+}
--- src/Wellcome.Dds/Wellcome.Dds/Manifestation.cs
+++ src/Wellcome.Dds/Wellcome.Dds/Manifestation.cs
@@ -9,11 +9,37 @@ namespace Wellcome.Dds
     /// </summary>
     public class Manifestation
     {
-        public string Id { get; set; }
-        public string PackageIdentifier { get; set; }
-        public string ManifestationIdentifier { get; set; }
+        // FM = FlatManifestation, see https://github.com/wellcomelibrary/dds-ecosystem/blob/new-storage-service/wellcome-dds/Wellcome.Dds.Data/FlatManifestation.cs
+        
+        public string Id { get; set; } // this is string3, the same as the manifest in S3; bnnn_0003
+        public string PackageIdentifier { get; set; } // the b number, aka String1
+        public string ManifestationIdentifier { get; set; } // do we need this? Same as Id (String3)
+        public string VolumeIdentifier { get; set; } // legacy of Old model - aka String2
         public DateTime Processed { get; set; }
         public int PackageShortBNumber { get; set; }
-        public string Label { get; set; }
+        public string Label { get; set; } // RootSectionTitle in FM?
+        public string PackageLabel { get; set; } // RootSectionTitle in FM?  (which?) - allows us to have Manifest and Work Labels
+
+        public int Index { get; set; } // Manifestation in FM
+        public int FileCount { get; set; }
+        
+        
+        public bool SupportsSearch { get; set; }
+        public bool IsAllOpen { get; set; }
+        public string PosterImage { get; set; }
+        public string PermittedOperations { get; set; }
+        public string RootSectionAccessCondition { get; set; }
+        public string RootSectionType { get; set; }
+        public string FirstFileName { get; set; }
+        public string FirstFileStorageIdentifier { get; set; }
+        public string FirstFileExtension { get; set; }
+        public string DipStatus { get; set; }
+        public string ErrorMessage { get; set; }
+        public string PackageFile { get; set; }
+        public DateTime? PackageFileModified { get; set; }
+        public string ManifestationFile { get; set; }
+        public DateTime? ManifestationFileModified { get; set; }
+        public string AssetType { get; set; }
+        public string DlcsAssetType { get; set; }
     }
 }
