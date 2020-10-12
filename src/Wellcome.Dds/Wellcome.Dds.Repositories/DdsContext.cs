72d9ef67b9d5bdcb84480121bf256c61005e0e21
tomcrane <tom@nanoplanet.com>
2020-10-12 19:31:22 +0100

    undo addition to ddscontext

--- src/Wellcome.Dds/Wellcome.Dds.Repositories/DdsContext.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Repositories/DdsContext.cs
@@ -26,32 +26,6 @@ namespace Wellcome.Dds.Repositories
         public List<Manifestation> AutoComplete(string query)
         {
             throw new NotImplementedException();
-            //     const string BNumberSql =
-            //         "SELECT TOP 100 * FROM FlatManifestations where Manifestation=0 AND PackageIdentifier LIKE @query";
-            //     const string TitleSql =
-            //         "SELECT TOP 100 * FROM FlatManifestations where Manifestation=0 AND RootSectionTitle LIKE @query";
-            //     const string ImFeelingLuckySql =
-            //         "SELECT top 1 * from FlatManifestations where AssetType='seadragon/dzi' and Manifestation=0 order by NEWID()";
-            //
-            //     if (query == "imfeelinglucky")
-            //     {
-            //         return Database.SqlQuery<FlatManifestation>(ImFeelingLuckySql).ToList();
-            //     }
-            //
-            //     string sql;
-            //     SqlParameter queryParam = new SqlParameter("@query", SqlDbType.NVarChar);
-            //     if (Regex.IsMatch(query, "\\Ab\\d+\\z"))
-            //     {
-            //         queryParam.Value = string.Format("{0}%", query);
-            //         sql = BNumberSql;
-            //     }
-            //     else
-            //     {
-            //         queryParam.Value = string.Format("%{0}%", query);
-            //         sql = TitleSql;
-            //     }
-            //     return Database.SqlQuery<FlatManifestation>(sql, queryParam).ToList();
-            // }
         }
     }
 }
