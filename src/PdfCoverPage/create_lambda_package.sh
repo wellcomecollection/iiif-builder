eb5545543d6ded7578597c8a43b2e466a3f51e38
Donald Gray <donald.gray@digirati.com>
2021-01-28 17:04:36 +0000

    Use pip3 rather than pip in command

--- src/PdfCoverPage/create_lambda_package.sh
+++ src/PdfCoverPage/create_lambda_package.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # see https://docs.aws.amazon.com/lambda/latest/dg/python-package.html#python-package-dependencies
-pip install --system --target ./package reportlab
+pip3 install --system --target ./package reportlab
 cd package ; zip -r ../pdf_gen.zip . * ; cd ..
 zip -g pdf_gen.zip generator.py
 zip -g pdf_gen.zip fonts/Inter-Bold.ttf
