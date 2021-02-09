89dc75e7dce24cf73fe3e6c390e985ba72ac587d
tomcrane <tom@nanoplanet.com>
2021-02-09 16:45:32 +0000

    Add DB Migration: AddJobOptionsFlagsInt

--- src/Wellcome.Dds/Wellcome.Dds.AssetDomainRepositories/Migrations/20210209164435_AddJobOptionsFlagsInt.Designer.cs
+++ src/Wellcome.Dds/Wellcome.Dds.AssetDomainRepositories/Migrations/20210209164435_AddJobOptionsFlagsInt.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Wellcome.Dds.AssetDomainRepositories;
@@ -9,9 +10,10 @@ using Wellcome.Dds.AssetDomainRepositories;
 namespace Wellcome.Dds.AssetDomainRepositories.Migrations
 {
     [DbContext(typeof(DdsInstrumentationContext))]
-    partial class DdsInstrumentationContextModelSnapshot : ModelSnapshot
+    [Migration("20210209164435_AddJobOptionsFlagsInt")]
+    partial class AddJobOptionsFlagsInt
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Wellcome.Dds/Wellcome.Dds.AssetDomainRepositories/Migrations/20210209164435_AddJobOptionsFlagsInt.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Wellcome.Dds.AssetDomainRepositories.Migrations
+{
+    public partial class AddJobOptionsFlagsInt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "workflow_options",
+                table: "workflow_jobs",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "workflow_options",
+                table: "workflow_jobs");
+        }
+    }
+}
