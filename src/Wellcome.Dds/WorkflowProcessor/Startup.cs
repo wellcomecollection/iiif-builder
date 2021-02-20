fc051513cd51cbf170f2c8a0cbc23400eb1088bf
Tom Crane <tom@nanoplanet.com>
2021-02-20 13:55:14 +0000

    remove refs to PdfService

--- src/Wellcome.Dds/WorkflowProcessor/Startup.cs
+++ src/Wellcome.Dds/WorkflowProcessor/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using OAuth2;
-using PdfService;
 using Utils.Aws.S3;
 using Utils.Caching;
 using Utils.Storage;
