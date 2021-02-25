de4ac77ab0b6129544d5e17bf6597a3d703edb56
Donald Gray <donald.gray@digirati.com>
2021-02-25 17:17:23 +0000

    Add uv services to p2 manifest

---
+++ src/Wellcome.Dds/Wellcome.Dds.Repositories.Tests/Presentation/V2/LegacyServiceFactoryTests.cs
@@ -0,0 +1,62 @@
+﻿using FluentAssertions;
+using IIIF.Presentation.V3.Content;
+using Wellcome.Dds.Repositories.Presentation;
+using Wellcome.Dds.Repositories.Presentation.V2;
+using Xunit;
+
+namespace Wellcome.Dds.Repositories.Tests.Presentation.V2
+{
+    public class LegacyServiceFactoryTests
+    {
+        [Fact]
+        public void GetLegacyService_Null_IfProfileUnknown()
+        {
+            // Arrange
+            var resource = new ExternalResource("Test") {Profile = "unknown"};
+            
+            // Act
+            var service = LegacyServiceFactory.GetLegacyService("b19818786", resource);
+            
+            // Assert
+            service.Should().BeNull();
+        } 
+        
+        [Fact]
+        public void GetLegacyService_TrackingExtension()
+        {
+            // Arrange
+            var resource = new ExternalResource("Test")
+            {
+                Profile = Constants.Profiles.TrackingExtension,
+                Label = Lang.Map("Test Label")
+            };
+            
+            // Act
+            var service = LegacyServiceFactory.GetLegacyService("b19818786", resource);
+            
+            // Assert
+            service.Should()
+                .BeOfType<TrackingExtensionsService>()
+                .Which.TrackingLabel.Should().Be("Test Label");
+        }
+        
+        [Fact]
+        public void GetLegacyService_AccessControlHints()
+        {
+            // Arrange
+            var resource = new ExternalResource("Test")
+            {
+                Profile = Constants.Profiles.AccessControlHints,
+                Label = Lang.Map("Test Label")
+            };
+            
+            // Act
+            var service = LegacyServiceFactory.GetLegacyService("b19818786", resource);
+            
+            // Assert
+            service.Should()
+                .BeOfType<AccessControlHints>()
+                .Which.AccessHint.Should().Be("Test Label");
+        }  
+    }
+}
---
+++ src/Wellcome.Dds/Wellcome.Dds.Repositories/Presentation/V2/AccessControlHints.cs
@@ -0,0 +1,27 @@
+﻿using IIIF;
+using IIIF.Presentation.V2;
+using IIIF.Presentation.V3.Strings;
+using Newtonsoft.Json;
+using Wellcome.Dds.Common;
+
+namespace Wellcome.Dds.Repositories.Presentation.V2
+{
+    /// <summary>
+    /// Service user to for by UV for legacy P2 manifests.
+    /// </summary>
+    public class AccessControlHints : ResourceBase, IService
+    {
+        public override string? Type { get; set; } = null;
+        
+        [JsonProperty(Order = 5, PropertyName = "accessHint")]
+        public string AccessHint { get; }
+
+        public AccessControlHints(DdsIdentifier identifier, LanguageMap accessHint)
+        {
+            AccessHint = accessHint.ToString();
+            Id = $"https://wellcomelibrary.org/iiif/{identifier}/access-control-hints-service";
+            Profile = Constants.Profiles.TrackingExtension;
+            Context = "http://wellcomelibrary.org/ld/iiif-ext/0/context.json";
+        }
+    }
+}
