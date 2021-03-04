0cfd4af6c51080b8f12158960cd0dcb8dd1ae1a0
tomcrane <tom@nanoplanet.com>
2021-03-04 17:19:19 +0000

    Poster becomes ThumbController as now handles all b number thumbs

--- src/Wellcome.Dds/Wellcome.Dds.Server/Controllers/ThumbController.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Server/Controllers/ThumbController.cs
@@ -4,20 +4,31 @@ using SixLabors.ImageSharp;
 using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Processing;
+using Utils;
 using Utils.Web;
 using Wellcome.Dds.AssetDomain.Dlcs;
 using Wellcome.Dds.AssetDomain.Mets;
 using Wellcome.Dds.Common;
+using Wellcome.Dds.IIIFBuilding;
+using Wellcome.Dds.Repositories;
+using Wellcome.Dds.Repositories.Presentation;
 
 namespace Wellcome.Dds.Server.Controllers
 {
-    public class PosterImageController : Controller
+    public class ThumbController : Controller
     {
         private readonly IMetsRepository metsRepository;
+        private DdsContext ddsContext;
+        private UriPatterns uriPatterns;
         
-        public PosterImageController(IMetsRepository metsRepository)
+        public ThumbController(
+            IMetsRepository metsRepository,
+            DdsContext ddsContext,
+            UriPatterns uriPatterns)
         {
             this.metsRepository = metsRepository;
+            this.ddsContext = ddsContext;
+            this.uriPatterns = uriPatterns;
         }
         
         [Route("thumbs/{id}")]
@@ -25,6 +36,31 @@ namespace Wellcome.Dds.Server.Controllers
         public async Task<IActionResult> Index(string id)
         {
             // TODO - if it's a BD PDF, return the large thumb or a resize of it or an info.json
+            var ddsId = new DdsIdentifier(id);
+            var manifestation = await ddsContext.Manifestations.FindAsync(id);
+            // this will also work for plain b number
+            if (manifestation == null)
+            {
+                manifestation = ddsContext.Manifestations
+                    .FirstOrDefault(m => m.PackageIdentifier == ddsId.BNumber);
+            }
+
+            if (manifestation == null)
+            {
+                return NotFound($"No thumbnail for {id}");
+            }
+
+            var iiifThumbs = manifestation.GetThumbnail();
+            if (iiifThumbs.HasItems())
+            {
+                return RedirectPermanent(iiifThumbs[0].Id);
+            }
+            
+            // TODO - PDF Cover thumbs - use Cantaloupe!
+            if (manifestation.AssetType == "image/jp2")
+            {
+                return NotFound($"No thumbnail for {id}");
+            }
             
             IMetsResource resource = await metsRepository.GetAsync(id);
             if (resource is ICollection multipleManifestation)
