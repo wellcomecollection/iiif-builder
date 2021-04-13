337ea1b23761e1f6cd52c185e8829ccebdc90a55
Donald Gray <donald.gray@digirati.com>
2021-04-13 10:40:58 +0100

    Add control_flow entity to instrumentation context
    This will replace the S3 implementation of sentinel

---
+++ src/Wellcome.Dds/Wellcome.Dds.AssetDomainRepositories/Migrations/20210413093342_Add control_flow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace Wellcome.Dds.AssetDomainRepositories.Migrations
+{
+    public partial class Addcontrol_flow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "control_flows",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    heartbeat = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    created_on = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    stopped_on = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_control_flows", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "control_flows");
+        }
+    }
+}
