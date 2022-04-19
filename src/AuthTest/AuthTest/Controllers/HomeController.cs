488d183bf98c5e53974cf3f15fcf91d46b31b114
Donald Gray <donald.gray@digirati.com>
2022-04-19 10:27:34 +0100

    Remove logger from homepage

--- src/AuthTest/AuthTest/Controllers/HomeController.cs
+++ src/AuthTest/AuthTest/Controllers/HomeController.cs
@@ -1,19 +1,11 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using AuthTest.Models;
 
 namespace AuthTest.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Index()
         {
