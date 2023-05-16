6bfa89b44bfe3790e13cc8410dcfdebeddf324ae
tomcrane <tom@nanoplanet.com>
2023-05-16 11:42:10 +0100

    return a fake accepted job from old GoobiCall endpoint

--- src/Wellcome.Dds/Wellcome.Dds.Server/Controllers/WorkflowController.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Server/Controllers/WorkflowController.cs
@@ -34,50 +34,20 @@ namespace Wellcome.Dds.Server.Controllers
         /// <param name="id">bNumber to create job for.</param>
         /// <returns>202 if accepted, else error.</returns>
         [HttpGet]
+        [Obsolete]
         [ProducesResponseType(202)]
         public async Task<ActionResult> Process(string id)
         {
             logger.LogInformation($"Received workflow/process instruction for {id}");
-
-            if (id.Equals(KnownIdentifiers.ChemistAndDruggist, StringComparison.InvariantCultureIgnoreCase))
+            logger.LogWarning("DDS will no longer accept workflow jobs on this endpoint. Use SQS instead.");
+            
+            // We will retain the format of the response until Goobi retires this call.
+            var fakeJob = new WorkflowJob
             {
-                const string message = "You can't rebuild Chemist and Druggist this way.";
-                logger.LogWarning(message);
-                return StatusCode(403, message);
-            }
-            try
-            {
-                var ddsId = new DdsIdentifier(id);
-                var message = new WorkflowMessage
-                {
-                    Identifier = ddsId.PackageIdentifier,
-                    Origin = "workflow-controller",
-                    Space = ddsId.StorageSpace,
-                    TimeSent = DateTime.UtcNow
-                };
-                var workflowJob = await workflowCallRepository.CreateWorkflowJob(message);
-                Response.AppendStandardNoCacheHeaders();
-                logger.LogInformation($"Accepted workflow/process instruction for {id}");
-                return StatusCode(202, workflowJob);
-            }
-            catch (Exception ex)
-            {
-                logger.LogError(ex, "Error creating WorkflowJob for bnumber '{bNumber}'", id);
-                var errorResponse = new WorkFlowCallError
-                {
-                    Message = ex.Message,
-                    StackTrace = ex.StackTrace
-                };
-                logger.LogError(errorResponse.Message, ex);
-                return StatusCode(500, errorResponse);
-            }
-        }
-        
-        class WorkFlowCallError
-        {
-            public string Type => "Error";
-            public string Message { get; set; }
-            public string StackTrace { get; set; }
+                Created = DateTime.UtcNow,
+                Identifier = id
+            };
+            return StatusCode(202, fakeJob);
         }
     }
 }
