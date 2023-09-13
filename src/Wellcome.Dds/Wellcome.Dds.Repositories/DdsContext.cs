fdd4fc608575724f02739623c81b5484c0c4ba37
tomcrane <tom@nanoplanet.com>
2023-09-13 15:51:20 +0100

    remove unused import

--- src/Wellcome.Dds/Wellcome.Dds.Repositories/DdsContext.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Repositories/DdsContext.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Common;
-using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Utils;
