5c2e72c120a2dd8805a76ab2fd987434e2cf7dc7
tomcrane <tom@nanoplanet.com>
2025-08-07 10:47:52 +0100

    Move ForwardedHeadersOptions to host-called Configure(..)

--- src/Wellcome.Dds/Wellcome.Dds.Dashboard/Startup.cs
+++ src/Wellcome.Dds/Wellcome.Dds.Dashboard/Startup.cs
@@ -143,6 +143,15 @@ namespace Wellcome.Dds.Dashboard
                 .AddDbContextCheck<DdsInstrumentationContext>("DdsInstrumentation-db")
                 .AddDbContextCheck<DdsContext>("Dds-db")
                 .AddUrlGroup(new Uri(dlcsOptions.ApiEntryPoint), "DLCS API");
+
+            services.Configure<ForwardedHeadersOptions>(opts =>
+            {
+                // This maintains the behaviour that was present in
+                // dotnet until .NET 8.0.17 + .NET 9.0.6 release and so avoids breaking changes.
+                opts.ForwardedHeaders = ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto;
+                opts.KnownNetworks.Clear();
+                opts.KnownProxies.Clear();
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -158,16 +167,8 @@ namespace Wellcome.Dds.Dashboard
                 app.UseDeveloperExceptionPage();
             }
 
-            // This is required for ADAuth on linux containers. When hosting in ECS we are doing ssl termination
-            // at load-balancer, so by default redirect will be http - this ensures https
-            var forwardedHeadersOpts = new ForwardedHeadersOptions
-            {
-                ForwardedHeaders = ForwardedHeaders.XForwardedHost | ForwardedHeaders.XForwardedProto
-            };
-            forwardedHeadersOpts.KnownNetworks.Clear();
-            forwardedHeadersOpts.KnownProxies.Clear();
-            app.UseForwardedHeaders(forwardedHeadersOpts);
-
+            app.UseForwardedHeaders();
+            
             app.UsePathBase("/dash");
             app.UseStaticFiles();
             app.UseRouting();
